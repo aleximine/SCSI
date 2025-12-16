dd05c4278026080ac19e1316383794ec41176b76
aleximine <mrfreezejr@gmail.com>
2025-12-16 13:31:23 -0500

    Import of old GitHub files
    I was adding commits from the wrong account. I will soon add the updated site from Neocities, though I have to change a lot of the code as things work differently with HTML links there.

---
+++ assets/js/loadingDynamics.js
@@ -0,0 +1,6 @@
+// Animations while site loads
+// function loadStart()
+// { document.getElementById("loadingStatus").innerHTML = "Terminal loading..."; }
+
+function loadComplete()
+{ document.getElementById("loadingStatus").innerHTML = "Terminal loading...\nLoad complete."; }
